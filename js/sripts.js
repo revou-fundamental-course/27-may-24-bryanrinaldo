7baed841c8de626257e14b3a7c8df60dad9fbffa
Bryan Rinaldo <bryan.rinaldo@gmail.com>
2024-06-05 22:44:23 +0700

    docs(changelog): update html, css, js

--- js/sripts.js
+++ js/sripts.js
@@ -1,1 +1,22 @@
 //This is javascript
+
+function validateForm() {
+    const nama = document.forms['message-form']['name-input'].value
+    if (nama == ''){
+        document.getElementById("error-name").innerHTML = "Tidak boleh kosong!"
+        return false
+    }
+}
+
+// menggunakan get elemen
+function formValidation() {
+    let nameInput = document.getElementById('name-input').value;
+    console.log(nameInput);
+}
+
+// menggunakan query selector
+// function formValidation() {
+//     let nameInput = document.querySelector('#name-input');
+//     console.log(nameInput);
+// }
+
